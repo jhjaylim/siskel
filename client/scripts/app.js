ee88cf1277c7e35fa35f8b6ca61beabc5c27911c
jhjaylim <jhjaylim@gmail.com>
2017-07-17 17:25:47 -0700

    siskel sorting

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -15,13 +15,13 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.on('change:comparator', this.sort, this);
   }
 
 });
